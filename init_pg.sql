0a540562612f46a42895bdfd03263fa10e3d6a6b
Andrew Dai <a@andrewdai.co>
2018-02-23 14:26:13 -0500

    Quote escape username and dbname

--- init_pg.sql
+++ init_pg.sql
@@ -1,5 +1,5 @@
 -- "username" and "dbname" are variables that must be
 -- passed in from the commandline
 -- ex) psql "$POSTGRES_URL" -v username="$USERNAME" -v dbname="$DBNAME" -f init_pg.sql
-CREATE USER :username;
-CREATE DATABASE :dbname WITH OWNER :username;
+CREATE USER :"username";
+CREATE DATABASE :"dbname" WITH OWNER :"username";
